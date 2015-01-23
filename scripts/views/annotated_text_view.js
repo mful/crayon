c0826152ffca91f569139e3c6973dcd64e892695
Matt <fulton.matthew@gmail.com>
2015-01-23 09:32:44 -0500

    For history: find nodes from text content using XPath. Did not work on some text nodes, dont know why

--- scripts/views/annotated_text_view.js
+++ scripts/views/annotated_text_view.js
@@ -11,19 +11,21 @@ crayon.views.AnnotatedTextView = ( function () {
     var elements, containingNode, matchRegex, node, i;
     if ( this.rendered ) return this;
 
-    elements = this.findAnnotationElements();
+    elements = this.findAnnotationElements2();
 
     for ( i = 0; i < elements.length; i++ ) {
-      matchRegex = new RegExp( crayon.helpers.utility.regexEscape(elements[i].matchStr) );
-      containingNode = crayon.helpers.utility.find(
-        elements[i].element.childNodes,
-        function ( node ) {
-          return !!node.nodeValue && !!node.nodeValue.match( matchRegex );
-        }
-      );
+      matchRegex = new RegExp( crayon.helpers.utility.regexEscape(elements[i].matchStr.trim()) );
+      // containingNode = crayon.helpers.utility.find(
+      //   elements[i].element.childNodes,
+      //   function ( node ) {
+      //     return !!node.nodeValue && !!node.nodeValue.match( matchRegex );
+      //   }
+      // );
 
-      newNode = this._createModifiedNode( containingNode, elements[i].matchStr );
-      elements[i].element.replaceChild( newNode, containingNode );
+      // newNode = this._createModifiedNode( containingNode, elements[i].matchStr.replace(/\s+$/, '') );
+      newNode = this._createModifiedNode( elements[i].element, elements[i].matchStr.trim() );
+
+      elements[i].element.parentElement.replaceChild( newNode, elements[i].element );
     }
 
     this.rendered = true;
@@ -31,43 +33,62 @@ crayon.views.AnnotatedTextView = ( function () {
     return this;
   };
 
-  AnnotatedTextView.prototype.findAnnotationElements = function () {
-    var results, sentences, i, j, parentNode, sentResults, rawResults;
+  // AnnotatedTextView.prototype.findAnnotationElements = function () {
+  //   var results, sentences, i, j, parentNode, sentResults, rawResults, testText;
+  //   parentNode = this.model.selectedNode ? this.model.selectedNode : document.body;
+  //   results = [];
+
+  //   // first try by full text
+  //   rawResults = this._getDOMNodesFromText( this.model.attributes.text, parentNode );
+
+  //   if ( rawResults.snapshotLength < 1 ) {
+  //     // next try by full sentences
+  //     sentences = crayon.helpers.utility.separateSentences( this.model.attributes.text );
+  //     sentResults = [];
+
+  //     for ( i = 0; i < sentences.length; i++ ) {
+  //       this._appendResult( results, sentences[i], parentNode );
+  //     }
+  //   } else {
+
+  //     // TODO: handle multiples -- need more views. Extract to lib?
+  //     for ( j = 0; j < rawResults.snapshotLength; j++ ) {
+  //       results.push(
+  //         {
+  //           element: rawResults.snapshotItem( j ),
+  //           matchStr: this.model.attributes.text
+  //         }
+  //       );
+  //     }
+
+  //   }
+
+  //   return results;
+  // };
+
+  AnnotatedTextView.prototype.findAnnotationElements2 = function () {
+    var results, sentences, i, j, parentNode, sentResults, rawResults, testText;
     parentNode = this.model.selectedNode ? this.model.selectedNode : document.body;
     results = [];
 
     // first try by full text
-    rawResults = this._getDOMNodesFromText( this.model.attributes.text, parentNode );
+    rawResults = this._getDOMNodesFromText2( this.model.attributes.text, parentNode );
 
-    if ( rawResults.snapshotLength < 1 ) {
+    if ( rawResults.length < 1 ) {
       // next try by full sentences
       sentences = crayon.helpers.utility.separateSentences( this.model.attributes.text );
       sentResults = [];
 
       for ( i = 0; i < sentences.length; i++ ) {
-        rawResults = this._getDOMNodesFromText( sentences[i] );
-        switch ( rawResults.snapshotLength ) {
-          case 1:
-            results.push({
-              element: rawResults.snapshotItem(0),
-              matchStr: sentences[i].trim()
-            });
-            break;
-          case 0:
-            // check by word
-            break;
-          default:
-            // find all that are adjacent to the rest of the annotated text?
-            break;
-        }
+        this._appendResult2( results, sentences[i], parentNode );
       }
     } else {
 
       // TODO: handle multiples -- need more views. Extract to lib?
-      for ( j = 0; j < rawResults.snapshotLength; j++ ) {
+      for ( j = 0; j < rawResults.length; j++ ) {
         results.push(
           {
-            element: rawResults.snapshotItem( j ),
+            element: rawResults[j],
             matchStr: this.model.attributes.text
           }
         );
@@ -80,16 +101,88 @@ crayon.views.AnnotatedTextView = ( function () {
 
   // private
 
-  AnnotatedTextView.prototype._getDOMNodesFromText = function ( text, parentNode ) {
-    if ( !parentNode ) parentNode = document.body;
+  // AnnotatedTextView.prototype._appendResult = function ( results, testText, parentNode, leftovers ) {
+  //   if ( !leftovers ) leftovers = [];
+  //   var rawResults = this._getDOMNodesFromText( testText, parentNode );
 
-    return(
-      document.evaluate(
-        '//*[contains(text(), "' + text.trim() + '")]',
-        parentNode,
-        null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null
-      )
-    );
+  //   switch ( rawResults.snapshotLength ) {
+  //     case 1:
+  //       results.push({
+  //         element: rawResults.snapshotItem(0),
+  //         matchStr: testText.trim()
+  //       });
+
+  //       if ( leftovers.length > 0 )
+  //         return this._appendResult( results, leftovers.join(''), parentNode );
+
+  //       break;
+  //     case 0:
+  //       leftovers.unshift( testText.match(/\s\S*\s?$/)[0] );
+  //       testText = testText.trim().replace( /\s\S*$/, '' );
+  //       return this._appendResult( results, testText, parentNode, leftovers );
+  //     default:
+  //       // find all that are adjacent to the rest of the annotated text?
+  //       break;
+  //   }
+
+  //   return results;
+  // };
+
+  AnnotatedTextView.prototype._appendResult2 = function ( results, testText, parentNode, leftovers ) {
+    if ( !leftovers ) leftovers = [];
+    var rawResults = this._getDOMNodesFromText2( testText, parentNode );
+
+    switch ( rawResults.length ) {
+      case 1:
+        results.push({
+          element: rawResults[0],
+          matchStr: testText
+        });
+
+        if ( leftovers.length > 0 )
+          return this._appendResult2( results, leftovers.join(''), parentNode );
+
+        break;
+      case 0:
+        leftovers.unshift( testText.match(/\s\S*\s?$/)[0] );
+        testText = testText.trim().replace( /\s\S*$/, '' );
+        return this._appendResult2( results, testText, parentNode, leftovers );
+      default:
+        // find all that are adjacent to the rest of the annotated text?
+        console.log( 'MORE THAN ONE NODE' );
+        break;
+    }
+
+    return results;
+  };
+
+  // AnnotatedTextView.prototype._getDOMNodesFromText = function ( text, parentNode ) {
+  //   if ( !parentNode ) parentNode = document.body;
+
+  //   return(
+  //     document.evaluate(
+  //       "//*[contains(translate(text(), \"'’\", ''), '" + text.trim().replace(/['’]/g, '') + "')]",
+  //       parentNode,
+  //       null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null
+  //     )
+  //   );
+  // };
+
+  AnnotatedTextView.prototype._getDOMNodesFromText2 = function ( text, parentNode ) {
+    var matchRegex = new RegExp( crayon.helpers.utility.regexEscape(text.trim()) ),
+        results = [],
+        i;
+
+    if ( !this.nodes ) {
+      this.nodes = [];
+      crayon.helpers.dom.getBaseNodes( parentNode, this.nodes );
+    }
+
+    for ( i = 0; i < this.nodes.length; i++ ) {
+      if ( !!this.nodes[i].textContent.match(matchRegex) ) results.push( this.nodes[i] );
+    }
+
+    return results;
   };
 
   AnnotatedTextView.prototype._createModifiedNode = function ( node, matchStr ) {
