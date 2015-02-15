22a886cb54832a23ad0c15e97a831ea1ff00f5d3
Matt <fulton.matthew@gmail.com>
2015-02-15 12:31:55 -0500

    Trim unused helper methods

--- scripts/helpers/dom.js
+++ scripts/helpers/dom.js
@@ -3,11 +3,6 @@ crayon.helpers.dom = {};
 
 ( function ( namespace ) {
 
-  namespace.isChildOf = function ( parent, child ) {
-    var children = parent.querySelectorAll( '*' );
-    return crayon.helpers.utility.includes( children, child );
-  };
-
   namespace.getBaseTextNodes = function ( el, nodes ) {
     !el.hasChildNodes() && el.nodeType === 3 ? nodes.push( el ) : el.normalize();
     var children = el.childNodes;
